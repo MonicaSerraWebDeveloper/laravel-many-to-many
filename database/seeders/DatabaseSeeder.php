196337b2528693cc23e5f44756e13425a88f837a
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-06-09 13:13:19 +0200

    feat: add model technology and seed the new technologies table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\PortfolioTableSeeder;
+use Database\Seeders\TypesTableSeeder;
+use Database\Seeders\TechnologiesTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            PortfolioTableSeeder::class,
+            TypesTableSeeder::class,
+            TechnologiesTableSeeder::class,
+        ]);
     }
 }
---
+++ database/seeders/TechnologiesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Illuminate\Support\Str;
+
+class TechnologiesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['html', 'css', 'js', 'vue', 'php', 'mysql', 'laravel'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name, '-');
+            $newTechnology->save();
+        }
+    }
+}
